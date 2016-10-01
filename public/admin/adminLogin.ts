0243f69f1554c832602718d110bd02e0b0a5393f
joeeames <joeeames@gmail.com>
2016-10-01 22:16:45 +0100

    converted adminLogin to ES6 Class

--- public/admin/adminLogin.ts
+++ public/admin/adminLogin.ts
@@ -1,19 +1,26 @@
 angular.module('app').component('adminLogin', { 
   templateUrl: '/admin/adminLogin.html',
-  controller: function($location, currentIdentity, auth, toastr) {
-    this.loggedIn = currentIdentity.authenticated();
-    if(this.loggedIn) {
-      $location.path('/home');
+  controller: class adminLoginCtrl {
+    loggedIn: boolean;
+    email: string;
+    password: string;
+
+    constructor(private $location, private currentIdentity, private auth, private toastr) {
+      this.loggedIn = currentIdentity.authenticated();
+      if(this.loggedIn) {
+        $location.path('/home');
+      }
     }
-    this.login = function() {
-      auth.login({
+
+    login() {
+      this.auth.login({
         username: this.email,
         password: this.password
       }).then(function() {
-        $location.path('/home');
+        this.$location.path('/home');
       }, function(err) {
         toastr.error(err);
       })
     }
   }
-})
+});
